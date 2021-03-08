385705377941c460af5c499186ce1d1acd0d89a0
feng li <1121607244@qq.com>
2021-03-08 15:18:41 +0800

    master

--- twosum.cpp
+++ twosum.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i,j;
+        for(i=0;i<nums.size()-1;i++)
+        {
+            for(j=i+1;j<nums.size();j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                   return {i,j};
+                }
+            }
+        }
+        return {i,j};      
+    }
+};
+
 int main()
 {
-    cout << "Hello world" << endl;
+    Solution Solution1;
+    vector<int> nums ;
+    int num[4] = {7,9,11,15},target = 9;
+    
+    Solution1.twoSum(nums,target);
+
     return 0;
 }
